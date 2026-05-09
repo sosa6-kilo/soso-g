651d56ebb333c4c30aa0187a551fe8b41a9484b1
sosa6-kilo <savabaldykov89@gmail.com>
2026-05-09 15:34:32 +0400

    Delete backend directory

--- backend/controllers/ProductController.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once './config/database.php';
-
-$query = $pdo->query("SELECT * FROM products");
-
-$products = $query->fetchAll(PDO::FETCH_ASSOC);
-
-echo json_encode($products);
--- backend/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-header("Content-Type: application/json");
-header("Access-Control-Allow-Origin: *");
-
-require_once './config/database.php';
-require_once './routes/api.php';
